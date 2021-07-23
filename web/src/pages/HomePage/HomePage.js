f472424eeb45f997842f044b1988f4743be89f03
Thomas Morita <thomasmorita@thomass-air.myfiosgateway.com>
2021-07-23 17:56:05 -0400

    add tailwindcss

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -4,6 +4,10 @@ const HomePage = () => {
   return (
     <>
       <h1>boardie</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 rounded-md	">
+        Hover me
+      </button>
+
 
       <p>
         My default route is named <code>home</code>, link to me with `
